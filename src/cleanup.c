42fe4b9271b9ddcdc84a57a50bd599ea5dcc22e7
Dumitrita Gorceac <dgorceac@student.42nice.fr>
2026-02-24 14:04:15 +0100

    cleanup.c

---
+++ src/cleanup.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 13:46:16 by dgorceac          #+#    #+#             */
+/*   Updated: 2026/02/24 13:53:58 by dgorceac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	cleanup(t_data *data)
+{
+	int i;
+
+	i = 0;
+	while (i < data->number_of_philo)
+	{
+		pthread_mutex_destroy(&data->philos[i].fork);
+		pthread_mutex_destroy(&data->philos[i].meal_lock);
+		i++;
+	}
+	pthread_mutex_destroy(&data->alive_lock);
+	pthread_mutex_destroy(&data->print_lock);
+	free(data->philos);
+}
