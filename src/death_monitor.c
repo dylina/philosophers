201fd2be82d58956f0ca213b171cdd981c5b8da6
Dumitrita Gorceac <dgorceac@student.42nice.fr>
2026-02-25 16:42:00 +0100

    death monitor

--- src/death_monitor.c
+++ src/death_monitor.c
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 14:13:20 by dgorceac          #+#    #+#             */
-/*   Updated: 2026/02/24 15:25:18 by dgorceac         ###   ########.fr       */
+/*   Updated: 2026/02/25 16:37:48 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,50 @@
 int	death_monitor(t_data *data)
 {
 	int i;
-	t_philo *philos;
-
+	
 	while (data->alive == 1)
 	{
 		i = 0;
-		while (&data->philos[i] < &data->number_of_philo - 1)
+		while (i < data->number_of_philo)
 		{
-			if (&data->alive != 1)
-			{
-				//opeste simularea
-				print_state(&philos[i], "died\n");
-				return (0);
-			}
-			if (&data->nb_meal == &philos->nb_meals)
-			{
-				//opeste simularea
-				return (0);
-			}
+			// TODO: verific dacă philosophul i a murit
+            // folos. check_if_one_died
 			i++;
 		}
+		// TODO: verific dacă toți au mâncat destul. opțional?
+		usleep(1000);
 	}
-	usleep(100);
+	return (0);
+}
+
+void	set_alive(t_data *data, int n)
+{
+	pthread_mutex_lock(&data->alive_lock);
+	data->alive = n;
+	pthread_mutex_unlock(&data->alive_lock);
+}
+
+int	check_if_one_died(t_data *data, int i)
+{
+	long	last_meal_time;
+	long	current_time;
+	long	elapsed;
+	t_philo	*philos;
+
+	philos = &data->philos[i];
+	current_time = get_time();
+	pthread_mutex_lock(&philos->meal_lock);
+	last_meal_time = philos->last_meal;
+	pthread_mutex_unlock(&philos->meal_lock);
+	elapsed = current_time - last_meal_time;
+	if (elapsed > data->time_to_die)
+	{
+		set_alive(data, 0);
+		pthread_mutex_lock(&data->print_lock);
+		printf("%ld %d died\n", get_time() - data->start_time,
+			philos->id);
+		pthread_mutex_unlock(&data->print_lock);
+		return (1);
+	}
+	return (0);
 }
