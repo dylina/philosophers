3d2f75f0378f25732834122d3d638ef45c53dbc9
Dumitrita Gorceac <dgorceac@student.42nice.fr>
2026-02-26 15:58:44 +0100

    minor changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:12:50 by dgorceac          #+#    #+#             */
-/*   Updated: 2026/02/25 15:31:21 by dgorceac         ###   ########.fr       */
+/*   Updated: 2026/02/26 15:50:19 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int main(int argc, char **argv)
 {
-	t_data data;//? trebuie initializata alt fel
+	t_data data;
+	
 	if (argc < 5 || argc > 6)
 	{
 		printf("Error : args error\n");
@@ -24,11 +25,9 @@ int main(int argc, char **argv)
 		return 1;
 	if (init_data(&data))
 		return 1;
-	
 	create_threads(&data);
-    //death_monitor
-    join_threads(&data);
-    cleanup(&data);
-	
+	death_monitor(&data);
+	join_threads(&data);
+	cleanup(&data);
 	return 0;
 }
